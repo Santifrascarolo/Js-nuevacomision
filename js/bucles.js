8302cdf151ab42bcb7c26ec53b2fb54d925760ed
Santiago Frascarolo <santifrascarolo03@gmail.com>
2021-08-19 20:41:06 -0300

    arbol principal

---
+++ js/bucles.js
@@ -0,0 +1,37 @@
+/* 
+//
+while(condicion){
+    codigo a ejecutar si se cumple la condicion
+}
+
+*/
+let contador = 1;
+
+while(contador <= 100){
+    document.write("Elemento " + contador + "<br>")
+    contador++
+
+    //(otra opcion)
+    // contador = contador +1; 
+}
+
+// estructura Do-While
+/* 
+
+do{
+    codigo a ejecutar
+    cambio la condicion
+}while(condicion)
+
+*/
+
+
+// estructura For 
+/*
+
+for(crear la variable; condicion; incremento o decremento el valor de la variable){
+    codigo a ejecutar
+}
+
+*/
+
