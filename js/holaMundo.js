da61150f6cc57d45b1dcb6077ea5b692c597992a
Santiago Frascarolo <santifrascarolo03@gmail.com>
2021-08-19 16:11:32 -0300

    fin primera clase JavaScript

---
+++ js/holaMundo.js
@@ -0,0 +1,23 @@
+//comentario simple
+
+/* esto es un 
+comentario
+de varias lineas
+*/
+
+//mostrar un mensaje por consola
+console.log("hola")
+
+//mostrar un msj en la pagina html
+document.write("Hola!")
+
+// mostrar una ventana emergente
+alert("WARMING")
+
+// crear variables var - let - const
+let palabraBuscada = "Perro" ;
+
+document.write("<br>Variable palabra buscada: " + palabraBuscada)
+
+const comision = 17
+document.write("<br>Variable constante: " + comision)
